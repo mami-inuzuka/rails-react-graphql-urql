aba8d559fc2c145d9a93f01759095b74b2ae873e
mami-inuzuka <inuzuka.mami@gmail.com>
2022-11-03 11:43:14 +0900

    UpdateBookミューテーションの作成

---
+++ app/graphql/mutations/update_book.rb
@@ -0,0 +1,18 @@
+module Mutations
+  class UpdateBook < Mutations::BaseMutation
+    argument :id, ID, required: true
+    argument :params, InputTypes::Book, required: true
+
+    field :book, Types::BookType, null: false
+
+    def resolve(id:, params:)
+      book = Book.find(id)
+      book.update!(params.to_h)
+
+      {book: book}
+      
+    rescue => e
+      GraphQL::ExecutionError.new(e.message)
+    end
+  end
+end
