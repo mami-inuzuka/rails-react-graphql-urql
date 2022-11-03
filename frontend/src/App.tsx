c58929423e42ad968b3e6f1675ad1347aa32722f
mami-inuzuka <inuzuka.mami@gmail.com>
2022-11-04 07:06:44 +0900

    生成した型をもとにデータを問い合わせ出力してみる

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <div className="App"></div>;
+import React from "react";
+import { client } from ".";
+import { Book, BooksDocument } from "./generated/graphql";
+
+type State = {
+  books: Book[];
+};
+class App extends React.Component {
+  state: State = {
+    books: [],
+  };
+  // @ts-ignore
+  constructor(props) {
+    super(props);
+    this.init();
+  }
+
+  async init() {
+    const response = await client.query(BooksDocument, {}).toPromise();
+    this.setState({
+      books: response.data.books,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>書籍一覧</h1>
+        {this.state.books.map((book) => {
+          return <p key={book.id}>本のタイトル：{book.title}</p>;
+        })}
+      </>
+    );
+  }
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createClient } from "urql";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+export const client = createClient({
+  url: "http://localhost:3000/graphql",
+});
+
 root.render(
   <React.StrictMode>
     <App />
