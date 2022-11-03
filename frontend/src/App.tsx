4e87014a4fbac198ceda13f639473ca9052473e4
mami-inuzuka <inuzuka.mami@gmail.com>
2022-11-03 13:06:29 +0900

    自動生成したHookを用いて本の追加をできるようにする

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import { useBooksQuery } from "./graphql/generated";
+import { useState } from "react";
+import { useBooksQuery, useCreateBookMutation } from "./graphql/generated";
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
+  // refetchQueries ... mutation完了後に実行したいqueryを指定
+  const [createBook] = useCreateBookMutation({ refetchQueries: ["books"] });
+  const [title, setTitle] = useState("");
 
   return (
     <>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={() => {
+          createBook({ variables: { params: { title: title } } });
+          setTitle("");
+        }}
+      >
+        保存
+      </button>
       {books.map((book) => {
         return <div key={book.id}>{book.title}</div>;
       })}
